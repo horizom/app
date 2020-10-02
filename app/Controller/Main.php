63f9c87232b65cb4d4a240b349f29892fd0dde00
Roland Edi <lambirou225@gmail.com>
2020-10-02 09:25:43 +0000

    Commit 02-10-2020

--- app/Controller/Main.php
+++ app/Controller/Main.php
@@ -4,20 +4,14 @@ namespace App\Controller;
 
 use Horizom\Http\Request;
 use Horizom\Http\Response;
-use App\Model\User;
 
 class Main
 {
     public function __construct()
-    {
-        # code...
-    }
+    {}
 
     public function index(Request $request, Response $response, array $attributes)
     {
-        $user = User::where('id', 1)->first()->toArray();
-
-        $data['user'] = $user;
         $data['name'] = "Horizom Framawork";
 
         return $response->view('index', $data);
