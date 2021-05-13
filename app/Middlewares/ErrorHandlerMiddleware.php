2c8c5419f771461942885034c407335a0d088dc0
Hen Drick’s <62672799+Hen-Dricks@users.noreply.github.com>
2021-05-13 18:50:48 +0000

    update
    Bug fixes

--- app/Middlewares/ErrorHandlerMiddleware.php
+++ app/Middlewares/ErrorHandlerMiddleware.php
@@ -3,27 +3,30 @@
 namespace App\Middlewares;
 
 use Throwable;
-use Horizom\Error\ErrorHandlerInterface;
-use Horizom\Exception\MethodNotAllowedException;
-use Horizom\Exception\NotFoundException;
 use Psr\Http\Message\ResponseInterface;
+use Horizom\Exception\NotFoundException;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Horizom\Interfaces\ErrorHandlerInterface;
+use Horizom\Exception\MethodNotAllowedException;
 
 class ErrorHandlerMiddleware implements ErrorHandlerInterface
 {
-    public function handle(Throwable $e, ServerRequestInterface $request): ResponseInterface
+    public function handle(Throwable $e, ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if ($e instanceof NotFoundException) {
-            return response(404)->view('errors.error_404', ['message' => $e->getMessage()]);
+        {
+            if ($e instanceof NotFoundException) {
+                return response(404)->view('errors.error_404', ['message' => $e->getMessage()]);
+            }
+    
+            if ($e instanceof MethodNotAllowedException) {
+                return response(405, ['Allow' => $e->getAllowedMethods()])
+                    ->view('errors.error_405', ['message' => $e->getMessage()]);
+            }
+    
+            return response(500)->view('errors.error_500', [
+                'message' => "Internal Server Error"
+            ]);
         }
-
-        if ($e instanceof MethodNotAllowedException) {
-            return response(405, ['Allow' => $e->getAllowedMethods()])
-                ->view('errors.error_405', ['message' => $e->getMessage()]);
-        }
-
-        return response(500)->view('errors.error_500', [
-            'message' => "Internal Server Error"
-        ]);
     }
 }
